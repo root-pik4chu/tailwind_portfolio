728d9b05c5ffdf6b2397cb1516fbcae26f633a3f
root <68701068+root-pik4chu@users.noreply.github.com>
2025-11-21 01:17:46 +0530

    .jsx error in button_component import mistake

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ganesha from '../../assets/tech/ganesha.png'
 import { Link } from 'react-router-dom'
-import Button_component from './Button_Component'
+import Button_component from './Button_Component.jsx'
 const NavBar = () => {
   const driveUrl = "https://drive.google.com/file/d/1JipHP6kzQyIFky1dLzUR_98BRb6DOHu0/view";
   return (
