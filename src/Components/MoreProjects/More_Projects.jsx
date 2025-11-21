dfe87a2dd39524ab8e630c604ce1dec396f43fc5
root <68701068+root-pik4chu@users.noreply.github.com>
2025-11-21 14:49:29 +0530

    bit change thing

--- src/Components/MoreProjects/More_Projects.jsx
+++ src/Components/MoreProjects/More_Projects.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const More_Projects = () => {
   return (
     <div>
-      <div className="w-full h-screen flex items-center justify-center text-white">
-         updating soon...
+      <div className="w-full h-[300vh] flex items-center justify-center text-white overflow-y-hidden">
+          <div className="w-full h-full grid grid-cols-8 grid-overlay ">
+            
+          </div>
       </div>
     </div>
   )
